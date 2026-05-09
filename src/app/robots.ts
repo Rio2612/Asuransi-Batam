8ed85d6da6a1d2ff0438ccfb1f31f8cdda7197cf
Asuransi Kito <141880694+Rio2612@users.noreply.github.com>
2026-05-09 10:19:20 +0700

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
       },
     ],
     sitemap: "https://asuransibatam.biz.id/sitemap.xml",
-    host: "https://asuransibatam.biz.id",
+    
   };
 }
